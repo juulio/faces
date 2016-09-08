6f2767d25166d51450087c935147778dda65dabb
Julio Del Valle <julio.delvalle@possible.com>
2016-09-07 20:34:03 -0600

    Adding files

--- app/js/main.js
+++ app/js/main.js
@@ -10,17 +10,51 @@
  * @namespace
  */
 
-(function (context, $) {
+(function (context) {
 
 	'use strict';
 
-	var vars = {
+  function init() {
+    // Grab elements, create settings, etc.
+    var video = document.getElementById('video');
 
-	};
-
-    function init() {
+    // Get access to the camera!
+    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      // Not adding `{ audio: true }` since we only want video now
+      navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+          video.src = window.URL.createObjectURL(stream);
+          video.play();
+      });
     }
+    else if(navigator.getUserMedia) { // Standard
+    navigator.getUserMedia({ video: true }, function(stream) {
+        video.src = stream;
+        video.play();
+    }, errBack);
+} else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
+    navigator.webkitGetUserMedia({ video: true }, function(stream){
+        video.src = window.webkitURL.createObjectURL(stream);
+        video.play();
+    }, errBack);
+} else if(navigator.mozGetUserMedia) { // Mozilla-prefixed
+    navigator.mozGetUserMedia({ video: true }, function(stream){
+        video.src = window.URL.createObjectURL(stream);
+        video.play();
+    }, errBack);
+}
 
-    $(init);
+    // Elements for taking the snapshot
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext('2d');
+    var video = document.getElementById('video');
 
-}(project_name, jQuery));
+    // Trigger photo take
+    document.getElementById("snap").addEventListener("click", function() {
+      context.drawImage(video, 0, 0, 640, 480);
+    });
+
+  }
+
+  init();
+
+}(project_name));
